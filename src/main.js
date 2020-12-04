4f75426813cc34d1123454025cd3969dbe3cde95
1105434372@qq.com <1105434372@qq.com>
2020-12-04 14:25:48 +0800

    'first'

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/*
+*项目的启动入口
+*/
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 创建vue根实例
+// 把router配置到实例中
+// 通过render方法把app根组件渲染到实例中。
+new Vue({
+  router,
+  render: h => h(App)
+  // el:'#app'//等价于 $mount('#app')
+}).$mount('#app')
